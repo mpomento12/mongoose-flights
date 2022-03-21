b0de2eeb08e60d3422618553078ffc4e915ccb98
mpomento <mpomento@gmail.com>
2022-03-21 14:35:19 -0700

    new

---
+++ controllers/destinations.js
@@ -0,0 +1,14 @@
+// const Movie = require('../models/flight');
+
+// module.exports = {
+//   create
+// };
+
+// function create(req, res) {
+//    flight.findById(req.paras.id, function(err,movie) {
+//        flight.destinations.push(req.body);
+//        flight.save(function(err) {
+//            res.redirect(`flights/${movie.id}`);
+//        });
+//    }); 
+// }
---
+++ controllers/flights.js
@@ -0,0 +1,26 @@
+const Flight = require('../models/flight')
+
+module.exports = {
+    new: newFlight,
+    create,
+    index,
+}
+
+function index (req, res) {
+    Flight.find({}, function(err, flights){
+        res.render('flights/index', {flights})
+    });
+};
+
+function newFlight(req, res) {
+    res.render('flights/new');
+}
+
+function create (req, res) {
+    const flight = new Flight(req.body)
+    flight.save(function(err) {
+        if(err) return res.render('flights/new');
+        res.redirect('/flights/new');
+    });
+};
+
